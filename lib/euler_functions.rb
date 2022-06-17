b424ab954728e39d4b48b48b613b09608fb2e674
Benjamin Williams <deanpbr@gmail.com>
2022-06-17 16:24:11 -0400

    Initial commit

---
+++ lib/euler_functions.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "euler_functions/version"
+
+module EulerFunctions
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/euler_functions/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module EulerFunctions
+  VERSION = "0.1.0"
+end
